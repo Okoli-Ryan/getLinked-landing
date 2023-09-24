60e7dbaa562c54406515c3ff663cbca33924163f
okoli-ryan <okoliryan50@gmail.com>
2023-09-24 12:26:40 +0100

    moved assets to public folder

--- src/app/components/BodySection/Rewards/Rewards.tsx
+++ src/app/components/BodySection/Rewards/Rewards.tsx
@@ -1,53 +1,59 @@
 import Image from "next/image";
 import React from "react";
 
+import BronzeMedal from "@images/home/bronze_medal 1.png";
+import GoldMedal from "@images/home/gold_medal 1.png";
+import RewardImage from "@images/home/Reward.png";
+import SilverMedal from "@images/home/silver_medal 1.png";
+
 export default function Rewards() {
 	return (
 		<div className="container py-12">
-			<h3 className="text-xl font-bold text-center">
-				Prizes and <br />
-				<span className="text-primary">Rewards</span>
-			</h3>
-			<div className="flex flex-col gap-8">
-				<p className="text-sm text-center">Highlight of the prizes or rewards for winners and for participants.</p>
+			<div className="block md:hidden">
+				<h3 className="text-xl font-bold text-center md:text-3xl">
+					Prizes and <br />
+					<span className="text-primary">Rewards</span>
+				</h3>
+				<div className="flex flex-col gap-8">
+					<p className="text-sm text-center md:text-base">Highlight of the prizes or rewards for winners and for participants.</p>
+				</div>
 			</div>
-			<Image width={320} height={280} src="/images/home/Reward.png" className="w-full h-auto" alt="Rewards illustration" />
-			<div className="grid grid-cols-3 gap-4 mt-36">
-				<div className="w-full bg-[#D434FE1F] border-2 border-primary rounded-lg flex flex-col relative gap-2 pb-4 items-center h-max">
-					<Image
-						src="/images/home/silver_medal 1.png"
-						width={80}
-						height={80}
-						className="w-full absolute object-contain -top-[30%]"
-						alt="Silver medal"
-					/>
-					<h2 className="font-montserrat font-bold text-sm mt-16">2nd </h2>
-					<h2 className="font-montserrat font-semibold text-sm">Runner</h2>
-					<h2 className="text-primary font-montserrat font-bold text-sm">N 300,000</h2>
-				</div>
-				<div className="w-full bg-[#903AFF1F] border-2 border-[#903AFF] rounded-lg flex flex-col relative gap-2 pb-4 items-center h-max">
-					<Image
-						src="/images/home/gold_medal 1.png"
-						width={125}
-						height={125}
-						className="w-[150%] max-w-none absolute h-auto object-contain -top-[60%]"
-						alt="Gold medal"
-					/>
-					<h2 className="font-montserrat font-bold text-sm mt-20">1st </h2>
-					<h2 className="font-montserrat font-semibold text-sm">Runner</h2>
-					<h2 className="text-[#903AFF] font-montserrat font-bold text-sm">N 400,000</h2>
-				</div>
-				<div className="w-full bg-[#D434FE1F] border-2 border-primary rounded-lg flex flex-col relative gap-2 items-center pb-4 h-max">
-					<Image
-						src="/images/home/bronze_medal 1.png"
-						height={80}
-						width={80}
-						className="w-full absolute h-auto object-contain -top-[30%]"
-						alt="Bronze medal"
-					/>
-					<h2 className="font-montserrat font-bold text-sm mt-16">3rd </h2>
-					<h2 className="font-montserrat font-semibold text-sm">Runner</h2>
-					<h2 className="text-primary font-montserrat font-bold text-sm">N 150,000</h2>
+			<div className="flex  flex-col md:flex-row">
+				<Image src={RewardImage} className="w-full md:w-1/3 h-auto object-contain" alt="Rewards illustration" />
+				<div className="flex flex-col w-full md:w-2/3">
+					<div className="hidden md:block">
+						<h3 className="text-xl font-bold text-center md:text-3xl">
+							Prizes and <br />
+							<span className="text-primary">Rewards</span>
+						</h3>
+						<div className="flex flex-col gap-8">
+							<p className="text-sm text-center md:text-base">Highlight of the prizes or rewards for winners and for participants.</p>
+						</div>
+					</div>
+					<div className="grid grid-cols-3 gap-4 mt-36 w-full">
+						<div className="w-full max-w-[300px] bg-[#D434FE1F] border-2 border-primary rounded-lg flex flex-col relative gap-2 pb-4 items-center h-max">
+							<Image src={SilverMedal} className="w-full absolute object-contain -top-[3rem] md:-top-[50%] md:max-w-[15rem]" alt="Silver medal" />
+							<h2 className="font-montserrat font-bold text-sm mt-16 md:mt-[60%]">2nd </h2>
+							<h2 className="font-montserrat font-semibold text-sm">Runner</h2>
+							<h2 className="text-primary font-montserrat font-bold text-sm">N 300,000</h2>
+						</div>
+						<div className="w-full max-w-[300px] bg-[#903AFF1F] border-2 border-[#903AFF] rounded-lg flex flex-col relative gap-2 pb-4 items-center h-max">
+							<Image
+								src={GoldMedal}
+								className="w-auto max-w-none absolute h-3/5 max-h-[10rem] object-contain -top-[5rem] md:-top-[10rem] md:max-w-[15rem]"
+								alt="Gold medal"
+							/>
+							<h2 className="font-montserrat font-bold text-sm mt-20 md:mt-[60%]">1st </h2>
+							<h2 className="font-montserrat font-semibold text-sm">Runner</h2>
+							<h2 className="text-[#903AFF] font-montserrat font-bold text-sm">N 400,000</h2>
+						</div>
+						<div className="w-full max-w-[300px] bg-[#D434FE1F] border-2 border-primary rounded-lg flex flex-col relative gap-2 items-center pb-4 h-max">
+							<Image src={BronzeMedal} className="w-full absolute object-contain -top-[3rem] md:-top-[50%] md:max-w-[15rem]" alt="Bronze medal" />
+							<h2 className="font-montserrat font-bold text-sm mt-16 md:mt-[60%]">3rd </h2>
+							<h2 className="font-montserrat font-semibold text-sm">Runner</h2>
+							<h2 className="text-primary font-montserrat font-bold text-sm">N 150,000</h2>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
